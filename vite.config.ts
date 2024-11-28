8347bb2ff914d87fe6333685250fc3977e3a11b1
Karthilenix <kk2521karthi@gmail.com>
2024-11-28 19:18:48 +0530

    Modify vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
+    root: path.resolve(__dirname, 'public'),
+    publicDir: path.resolve(__dirname, 'public'),
     build: {
-      outDir: 'dist',
+      outDir: path.resolve(__dirname, 'dist'),
       sourcemap: true,
+      emptyOutDir: true
     },
     define: {
       'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)

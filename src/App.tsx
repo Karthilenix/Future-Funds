2910298fcbbf2e4930671308bf45860f761d29bf
Karthilenix <kk2521karthi@gmail.com>
2024-12-10 14:24:59 +0530

    Try-6

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <main>
+      <Navbar />
+      <main className="container mx-auto px-4 py-8">
         <Outlet />
       </main>
+      <Toaster position="top-right" />
     </div>
   );
 }
